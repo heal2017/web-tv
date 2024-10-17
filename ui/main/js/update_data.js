2ba74c0c9a811bf68ec3fc936368a00dea809384
heal2017 <488411891@qq.com>
2024-10-17 16:17:52 +0800

    老板初版

--- ui/main/js/update_data.js
+++ ui/main/js/update_data.js
@@ -43,10 +43,15 @@ async function fetchData() {
   var component4_data = JSON.parse(JSON.stringify(jsonData))['component4'];
   component4.setOption(component4_data);
 
-  // // component5
-  // var component5 = echarts.init(document.getElementById('component5'));
-  // var component5_data = JSON.parse(JSON.stringify(jsonData))['component5'];
-  // component5.setOption(component5_data);
+  // component5
+  var component5 = echarts.init(document.getElementById('component5'));
+  var component5_data = JSON.parse(JSON.stringify(jsonData))['component5'];
+  component5.setOption(component5_data);
+
+  // component6
+  var component6 = echarts.init(document.getElementById('component6'));
+  var component6_data = JSON.parse(JSON.stringify(jsonData))['component6'];
+  component6.setOption(component6_data);
 
   // // npi_ncn
   // var npi_ncn = echarts.init(document.getElementById('npi_ncn'));
